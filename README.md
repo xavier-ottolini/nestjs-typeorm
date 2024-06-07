080b5736bb8153de3029845167e81ba93b11dd8a
xavier-ottolini <xavophonic@yahoo.fr>
2024-06-07 12:00:38 +0200

    Update API documentation: add keyword

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Test d'un serveur Node.JS avec le framework Nest.Js et l'ORM TypeOpm
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
@@ -24,25 +25,28 @@
 
 ## Description
 
-[Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
+This project test the [Nest](https://github.com/nestjs/nest) framework TypeScript using [TypeOrm](https://typeorm.io) Object Relational Mapping.
 
-## Installation
+## Prerequisis
+
+This project uses Docker
+
+## Installation and running in dev mode
 
 ```bash
-$ npm install
+$ docker compose -f docker-compose-dev.yaml up -d --build
 ```
 
-## Running the app
+## Running the app in dev mode
 
 ```bash
-# development
-$ npm run start
+$ docker compose -f docker-compose.yaml start
+```
 
-# watch mode
-$ npm run start:dev
+## Stopping 
 
-# production mode
-$ npm run start:prod
+```bash
+$ docker compose -f docker-compose.yaml stop
 ```
 
 ## Test
@@ -58,16 +62,170 @@ $ npm run test:e2e
 $ npm run test:cov
 ```
 
-## Support
+## API
+### Users
+#### Lister les utilisateurs du forum
 
-Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
+- Method: GET
+- URL: `http://localhost:3000/users`
+
+#### Récupérer un utilisateur du forum
+
+- Method: GET
+- URL: `http://localhost:3000/users/{{id}}` où `{{id}}` est un nombre entier
+
+#### Supprimer un utilisateur du forum
+
+- Method: DELETE
+- URL:  `http://localhost:3000/users/{{id}}` où `{{id}}` est un nombre entier
+
+#### Ajouter un utilisateur au forum
+
+- Method: POST
+- Header: `Content-type: application/json`
+- URL `http://localhost:3000/users`
+- BODY:
+```json
+{
+  "firstname": "François",
+  "lastname":"Laconique",
+  "username":"francois.laconique",
+  "email":"francois.laconique@veonum.com"
+}
+```
+#### Modifier un utilisateur du forum
+
+- Method: PUT
+- URL: `http://localhost:3000/users/{{id}}` où `{{id}}` est un nombre entier
+- Header: `Content-type: application/json`
+- BODY:
+```json
+{
+  "username":"francois.laconique2",
+  "email":"francois.laconique2@veonum.com"
+}
+```
+### Keywords
+#### Lister les mots clés
+- Method: GET
+- URL `http://localhost:3000/keywords`
+
+#### Récupérer un mot clé
+
+- Method: GET
+- URL `http://localhost:3000/keyword{{id}}` où `{{id}}` est un nombre entier
+
+#### Créer un mot clé
+
+- Method: POST
+- Header: `Content-type: application/json`
+- URL `http://localhost:3000/keyword`
+- BODY
+```json 
+{
+  "label": "Astronaute"
+}
+```
+
+#### Modifier un mot clé
+
+- Method: PUT
+- Header: `Content-type: application/json`
+- URL `http://localhost:3000/keyword/{{id}}` où `{{id}}` est un nombre entier
+- BODY
+```json 
+{
+  "label": "Spacionaute"
+}
+```
+
+#### Supprimer un mot clé
+- Method: DELETE
+- URL `http://localhost:3000/keyword/{{id}}` où `{{id}}` est un nombre entier
+
+### Forum
+#### Lister les sujets du forum
+
+- Method: GET
+- URL `http://localhost:3000/forum`
+
+#### Récupérer un sujet du forum
+
+- Method: GET
+- URL: `http://localhost:3000/forum/{{id}}`
+
+#### Supprimer un sujet du forum
+
+- Method: DELETE
+- URL `http://localhost:3000/forum/{{id}}`
+
+#### Créer un sujet du forum
+
+- Method: POST
+- Header: `Content-type: application/json`
+- URL: `http://localhost:3000/forum`
+- BODY: 
+```json 
+{
+  "parent": null,
+  "title": "premier sujet",
+  "message": "salut à tous, j'ai un problème, je suis le premier homme sur la lune",
+  "user": 1,
+  "keywords": [{
+      "id": 1,
+    }, {
+      "id": 2,
+    }, {
+      "id": 3,
+    }]
+}
+```
+
+#### Ajouter un sujet du forum en réponse à un autre sujet
+
+- Method: POST
+- Header: `Content-type: application/json`
+- URL: `http://localhost:3000/forum`
+- BODY: 
+```json
+{
+  "parent": 1,
+  "title": "Re: premier sujet",
+  "message": "salut à toi! mais non, tu n'es pas tout seul ! moi aussi, je suis sur la lune !",
+  "user": 2,
+  "keywords": [{
+      "id": 1,
+      "label": "Satellite"
+    }, {
+      "id": 2,
+      "label": "Astronaute"
+    }, {
+      "id": 4,
+      "label": "Rencontre"
+    }]
+
+}
+```
+
+#### Modifier un sujet du forum
+
+- Method: PUT
+- URL: `http://localhost:3000/forum/{{id}}`
+- Header: `Content-type: application/json`
+- BODY:
+```json
+{
+  "title": "premier sujet modifié",
+  "message": "salut à tous, j'ai un problème, je suis le premier homme sur mars."
+}
+```
 
 ## Stay in touch
 
-- Author - [Kamil Myśliwiec](https://kamilmysliwiec.com)
-- Website - [https://nestjs.com](https://nestjs.com/)
-- Twitter - [@nestframework](https://twitter.com/nestframework)
+- Website - [https://nestjs.com](https://www.veonum.com//)
+- Author - [Xavier Ottolini](https://www.veonum.com/contact/)
 
 ## License
 
-Nest is [MIT licensed](LICENSE).
+Nest is [MIT licensed](https://raw.githubusercontent.com/nestjs/nestjs.com/master/LICENSE).
+TypeOrm is [MIT License](https://github.com/typeorm/typeorm/blob/master/LICENSE)
--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -17,58 +17,135 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       const hostname = 'test'
-      expect(appController.get("test")).toBe(`<html><head><title>Hello World!</title></head>
-      <body>
-      <h1>Hello World!</h1><h2>Lister les sujets du forum</h2>
-      <p>GET <a>http://${hostname}/forum</a></p>
-      <h2>Lister un sujet du forum</h2>
-      <p>GET <a>http://${hostname}/forum/\{\{id\}\}</a></p>
-      <h2>Supprimer un sujet du forum</h2>
-      <p>DELETE <a>http://${hostname}/forum/\{\{id\}\}</a></p>
-      <h2>Ajouter un sujet du forum</h2>
-      <p>POST <a>http://${hostname}/forum</a></p>  
-      <p>BODY: <pre>{
-        "parent": null,
-        "title": "premier sujet",
-        "message": "salut à tous, j'ai un problème, je suis le premier homme sur la lune",
-        "user": 1
-  }</pre></p>
-  <h2>Ajouter un sujet du forum avec un parent</h2>
-  <p>POST <a>http://${hostname}/forum</a></p>  
-  <p>BODY: <pre>{
-    "parent": 1,
-    "title": "Re: premier sujet",
-    "message": "salut à toi! mais non, tu n'es pas tout seul ! moi aussi, je suis sur la lune !",
-    "user": 2
-  
-  }</pre></p>
-  <h2>Modifier un sujet du forum</h2>
-  <p>PUT <a>http://${hostname}/forum/\{\{id\}\}</a></p>  
-  <p>BODY: <pre>{
-    "title": "premier sujet modifié",
-    "message": "salut à tous, j'ai un problème, je suis le premier homme sur mars."
-  }</pre></p>
-  <h2>Lister les utilisateurs du forum</h2>
+      expect(appController.get(hostname)).toBe(`<html>
+
+<head>
+  <style type="text/css">
+    body {
+      font-family: "Arial", "Helvetica", "sans-serif";
+    }
+
+    h1 {
+      text-align: center;
+    }
+
+    pre {
+      background-color: #eee;
+      padding: 0.5rem;
+    }
+  </style>
+  <title>Hello World!</title>
+</head>
+
+<body>
+  <h1>Hello World!</h1>
+  <h2>API Users</h2>
+  <h3>Lister les utilisateurs du forum</h3>
   <p>GET <a>http://${hostname}/users</a></p>
-  <h2>Lister un utilisateur du forum</h2>
+  <h3>Récupérer un utilisateur du forum</h3>
   <p>GET <a>http://${hostname}/users/\{\{id\}\}</a></p>
-  <h2>Supprimer un utilisateur du forum</h2>
+  <h3>Supprimer un utilisateur du forum</h3>
   <p>DELETE <a>http://${hostname}/users/\{\{id\}\}</a></p>
-  <h2>Ajouter un utilisateur au forum</h2>
-  <p>POST <a>http://${hostname}/users</a></p>  
-  <p>BODY: <pre>{
-    "firstname": "François",
-    "lastname":"Laconique",
-    "username":"francois.laconique",
-    "email":"francois.laconique@veonum.com"
-    }</pre></p>
-  <h2>Modifier un utilisateur du forum</h2>
-  <p>PUT <a>http://${hostname}/users/\{\{id\}\}</a></p>  
-  <p>BODY: <pre>{
-    "username":"francois.laconique2",
-    "email":"francois.laconique2@veonum.com"
-    }</pre></p>
-  </body>
+  <h3>Ajouter un utilisateur au forum</h3>
+  <p>POST <a>http://${hostname}/users</a></p>
+  <p>BODY:
+  <pre>{
+  "firstname": "François",
+  "lastname":"Laconique",
+  "username":"francois.laconique",
+  "email":"francois.laconique@veonum.com"
+  }</pre>
+  </p>
+  <h3>Modifier un utilisateur du forum</h3>
+  <p>PUT <a>http://${hostname}/users/\{\{id\}\}</a></p>
+  <p>BODY:
+  <pre>{
+  "username":"francois.laconique2",
+  "email":"francois.laconique2@veonum.com"
+  }</pre>
+  </p>
+  <h2>API keywords</h2>
+  <h3>Lister les mot clés</h3>
+  <p>GET <a>http://${hostname}/keywords</a></p>
+  <h3>Récupérer un mot clé</h3>
+  <p>GET <a>http://${hostname}/keywords/\{\{id\}\}</a></p>
+  <h3>Supprimer un mot clé</h3>
+  <p>DELETE <a>http://${hostname}/keywords/\{\{id\}\}</a></p>
+  <h3>Ajouter un mot clé</h3>
+  <p>POST <a>http://${hostname}/keywords</a></p>
+  <p>BODY:
+  <pre>{
+  "label": "Astronaute",
+  }</pre>
+  </p>
+  <h3>Modifier un mot clé</h3>
+  <p>PUT <a>http://${hostname}/keywords/\{\{id\}\}</a></p>
+  <p>BODY:
+  <pre>{
+  "label": "Spacionaute",
+  }</pre>
+  </p>    
+  <h2>API Forum</h2>
+  <h3>Lister les sujets du forum</h3>
+  <p>GET <a>http://${hostname}/forum</a></p>
+  <h3>Récupérer un sujet du forum</h3>
+  <p>GET <a>http://${hostname}/forum/\{\{id\}\}</a></p>
+  <h3>Supprimer un sujet du forum</h3>
+  <p>DELETE <a>http://${hostname}/forum/\{\{id\}\}</a></p>
+  <h3>Ajouter un sujet du forum</h3>
+  <p>POST <a>http://${hostname}/forum</a></p>
+  <p>BODY:
+  <pre>{
+  "parent": null,
+  "title": "premier sujet",
+  "message": "salut à tous, j'ai un problème, je suis le premier homme sur la lune",
+  "user": 1,
+  "keywords": [
+    {
+      "id": 1,
+      "label": "Satellite"
+    }, {
+      "id": 2,
+      "label": "Astronaute"
+    }, {
+      "id": 3,
+      "label": "Exploration"
+    }
+  ]
+}</pre>
+  </p>
+  <h3>Ajouter un sujet du forum avec un parent</h3>
+  <p>POST <a>http://${hostname}/forum</a></p>
+  <p>BODY:
+  <pre>{
+  "parent": 1,
+  "title": "Re: premier sujet",
+  "message": "salut à toi! mais non, tu n'es pas tout seul ! moi aussi, je suis sur la lune !",
+  "user": 2,
+  "keywords": [
+    {
+      "id": 1,
+      "label": "Satellite"
+    }, {
+      "id": 2,
+      "label": "Astronaute"
+    }, {
+      "id": 4,
+      "label": "Rencontre"
+    }
+  ]
+}</pre>
+  </p>
+  <h3>Modifier un sujet du forum</h3>
+  <p>PUT <a>http://${hostname}/forum/\{\{id\}\}</a></p>
+  <p>BODY:
+  <pre>{
+  "title": "premier sujet modifié",
+  "message": "salut à tous, j'ai un problème, je suis le premier homme sur mars."
+}</pre>
+  </p>
+</body>
+
 </html>
 `);
     });
